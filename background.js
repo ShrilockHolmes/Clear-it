6e70769c76a32c54431946de13f1168427efd017
Holmes <151185030+ShrilockHolmes@users.noreply.github.com>
2025-10-31 18:35:55 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,7 +1,11 @@
 let callback = function () {
   console.log('done');
 };
-
-// this function remove history of all time the callback function tells us what to do after it is done
-chrome.browsingData.removeHistory({
-}, callback);
+function start() {
+  // this function remove history of all time the callback function tells us what to do after it is done
+  chrome.browsingData.removeHistory({
+  }, callback)
+}
+//start();
+chrome.runtime.onStartup.addListener(start);
+chrome.runtime.onInstalled.addListener(start);
